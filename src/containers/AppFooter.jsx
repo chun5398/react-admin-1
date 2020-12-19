6d551c4fccd3cb058859de6fd4d9edd53167d4a1
淳杨乔 <chunyangqiao@yzwdeMacBook-Pro-87.local>
2020-12-20 00:35:08 +0800

    fix: 修改页脚 & 完成产品查询

--- src/containers/AppFooter.jsx
+++ src/containers/AppFooter.jsx
@@ -3,4 +3,4 @@ import { Layout } from 'antd'
 
 const { Footer } = Layout
 
-export default () => <Footer className='footer'>元乐拉伸后台管理系统 &copy;2020 - {new Date().getFullYear()}</Footer>
+export default () => <Footer className='footer'>元乐拉伸 &copy; 2020 - {new Date().getFullYear()}</Footer>
--- src/views/Product/Index.jsx
+++ src/views/Product/Index.jsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import CustomBreadcrumb from '@/components/CustomBreadcrumb'
-import { Layout } from 'antd'
+import { Layout, Divider, Table, Button } from 'antd'
 
 import axios from '../../api/index'
 import { API } from '../../api/config'
+import { SUCCESS } from '../../constants'
+
+const { Column } = Table
 
 const Product = () => {
     const [products, setProducts] = useState([])
-    const [pageNum, setPageNum] = useState(1)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [total, setTotal] = useState(0)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         axios
-            .post(API.PRODUCT.READ, { beginPage: pageNum, pageSize: 10 })
-            .then(res => {})
+            .post(API.PRODUCT.READ, { beginPage: currentPage, pageSize: 10 })
+            .then(res => {
+                setLoading(false)
+                if (res.code === SUCCESS) {
+                    setProducts(res.data.res)
+                    setTotal(res.data.total * 10)
+                }
+            })
             .catch(err => {})
     }, [])
 
-    useEffect(() => {}, [pageNum])
+    useEffect(() => {
+        axios
+            .post(API.PRODUCT.READ, { beginPage: currentPage, pageSize: 10 })
+            .then(res => {
+                if (res.code === SUCCESS) {
+                    setProducts(res.data.res)
+                    setTotal(res.data.total * 10)
+                }
+            })
+            .catch(err => {})
+    }, [currentPage])
 
     return (
         <Layout className={'button animated fadeIn'}>
@@ -24,7 +45,34 @@ const Product = () => {
                 <CustomBreadcrumb arr={['产品管理']}></CustomBreadcrumb>
             </div>
             <div className={'base-style'}>
-                <h3>产品管理</h3>
+                <Button type={'primary'}>新增产品</Button>
+                <Divider />
+                <Table
+                    loading={loading}
+                    dataSource={products}
+                    rowKey={record => record.id}
+                    pagination={{
+                        pageSize: 10,
+                        total: total,
+                        current: currentPage,
+                        onChange: (page, pageSize) => {
+                            setCurrentPage(page)
+                        }
+                    }}
+                    expandedRowRender={record => <p>{record.prodDesc}</p>}>
+                    <Column title={'ID'} dataIndex={'id'} />
+                    <Column title={'名称'} dataIndex={'prodName'} />
+                    <Column
+                        title={'图片'}
+                        dataIndex={'prodImgUrl'}
+                        render={(text, record) => (
+                            <img src={record.prodImgUrl} style={{ width: '100px', height: '50px' }} />
+                        )}
+                    />
+                    <Column title={'价格'} dataIndex={'prodPrice'} />
+                    <Column title={'时长'} dataIndex={'timeService'} render={record => <span>{record} 分钟</span>} />
+                    <Column title={'时长'} dataIndex={'timeService'} render={record => <span>{record} 分钟</span>} />
+                </Table>
             </div>
         </Layout>
     )
